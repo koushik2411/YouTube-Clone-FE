47625bd7c316111435123f9dce4324a4cc996faf
koushik2411 <koushikrk1324@gmail.com>
2026-06-10 23:10:18 +0530

    2nd commit: Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import VideoPlayer from './pages/VideoPlayer'
+import Channel from './pages/Channel'
+import CreateChannel from './pages/CreateChannel'
+
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path='/' element={<Home/>}/>
+
+        <Route path='/login' element={<Login/>}/>
+
+        <Route path='/register' element={<Register/>}/>
+
+        <Route path='/watch/:id' element={<VideoPlayer/>}/>
+
+        <Route path='/channel/:id' element={<Channel/>}/>
+
+        <Route path='/create-channel' element={<CreateChannel/>}/>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 

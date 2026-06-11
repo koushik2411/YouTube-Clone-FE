afafa33185dae8baf5746ac0dbc8d79608cbeff5
koushik2411 <koushikrk1324@gmail.com>
2026-06-11 10:35:08 +0530

    4th commit: Added skeleton of video card

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { FaCheckCircle, FaEye } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
-function VideoCard() {
+function VideoCard({ video }) {
   return (
-    <div>VideoCard</div>
+    <Link
+      to={`/watch/${video.id}`}
+      className=''
+    >
+
+      <div className=' border p-1 flex md:flex-col gap-5 md:gap-3'>
+
+        <img
+          src={video.thumbnailUrl}
+          alt={video.title}
+          className=' w-[35%] md:w-full rounded-lg'
+        />
+
+        <div className=' flex flex-col gap-1'>
+
+          <h2 className=' font-semibold'>{video.title}</h2>
+
+          <h3 className=' flex items-center gap-1.5 text-sm'>{video.channelName} <FaCheckCircle/> </h3>
+
+          <p className=' flex items-center gap-1.5 text-sm'><FaEye/> {video.views.toLocaleString()}</p>
+
+        </div>
+      </div>
+
+    </Link>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import FilterBar from '../components/FilterBar';
+import VideoCard from '../components/VideoCard';
+
+import videos from '../data/videos';
 
 function Home() {
 
@@ -18,6 +21,17 @@ function Home() {
         <div className=''>
 
           <FilterBar/>
+
+          <div className=' p-3 grid grid-cols-1 md:grid-cols-3 gap-3'>
+
+            {videos.map((video) => (
+              <VideoCard
+                key={video.id}
+                video={video}
+              />
+            ))}
+
+          </div>
 
         </div>
 

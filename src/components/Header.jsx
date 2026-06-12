4b32ca75f94bf4571df4541ea07f734b2ed552a7
koushik2411 <koushikrk1324@gmail.com>
2026-06-12 11:10:02 +0530

    13th commit: Modified header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaUser } from "react-icons/fa";
 import { AuthContext } from '../context/AuthContext';
 
 function Header({ search, setSearch, toggleSidebar }) {
@@ -10,12 +10,19 @@ function Header({ search, setSearch, toggleSidebar }) {
   return (
     <header className=' p-3 flex items-center justify-between'>
 
-        <div className='left flex items-center gap-3'>
+        <div className='left flex items-center gap-5'>
             <FaBars/>
 
-            <h1>
-                YouTube
-            </h1>
+            <div className=' flex items-center gap-1.5'>
+                <img
+                  src='../public/Youtube_logo.png'
+                  alt='logo'
+                  className=' h-5'
+                />
+
+                <h1 className=' font-bold hidden md:flex text-lg'>YouTube</h1>
+            </div>
+
         </div>
 
         {/* Search Bar */}
@@ -23,10 +30,10 @@ function Header({ search, setSearch, toggleSidebar }) {
 
             <input 
                 type='text'
-                placeholder='Search'
+                placeholder='🔎 Search'
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
-                className=''
+                className=' w-[40vw] py-0.5 px-2 rounded-2xl border-2 border-slate-400 outline-0'
             />
 
         </div>
@@ -37,8 +44,9 @@ function Header({ search, setSearch, toggleSidebar }) {
                 <h2>{user.username}</h2>
             ) : (
                 <Link to="/login">
-                    <button>
-                        Sign In
+                    <button className=' border-2 border-blue-400 text-blue-500 p-1 rounded-[50%] md:rounded-2xl flex items-center gap-1 cursor-pointer'>
+                        <FaUser/>
+                        <span className=' hidden md:flex'>Sign In</span>
                     </button>
                 </Link>
             )}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { FaHistory, FaHome } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdSubscriptions } from "react-icons/md";
+import { IoLibrary } from "react-icons/io5";
 
 function Sidebar() {
   return (
@@ -6,11 +11,19 @@ function Sidebar() {
 
       <ul>
 
-        <li>Home</li>
-        <li>Shorts</li>
-        <li>Subscriptions</li>
-        <li>History</li>
-        <li>Library</li>
+        <li>
+          <Link to="/" className=' p-2 flex items-center gap-2 cursor-pointer'>
+            <FaHome/> Home
+          </Link>
+        </li>
+
+        <li className=' p-2 flex items-center gap-2 cursor-pointer'> <SiYoutubeshorts /> Shorts</li>
+
+        <li className=' p-2 flex items-center gap-2 cursor-pointer'> <MdSubscriptions/> Subscriptions</li>
+
+        <li className=' p-2 flex items-center gap-2 cursor-pointer'> <FaHistory/> History</li>
+
+        <li className=' p-2 flex items-center gap-2 cursor-pointer'> <IoLibrary/> Library</li>
 
       </ul>
 

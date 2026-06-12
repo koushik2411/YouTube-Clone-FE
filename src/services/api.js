5832177b6c9416eaf049de466d313a0ad9005342
koushik2411 <koushikrk1324@gmail.com>
2026-06-12 23:02:57 +0530

    24th commit: updated backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5100/api",
+    baseURL: "https://youtube-clone-be-etbr.onrender.com/api",
 })
 
 export default api;
